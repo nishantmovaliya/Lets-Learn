85e8734ad218566a21141b3d87dcba731713e020
Nishant Movaliya <63806205+nishantmovaliya@users.noreply.github.com>
2021-07-31 11:32:58 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -4,6 +4,6 @@ class Sum{
     public static void main(String[] args) {
       int a = 5;
       int b =6;
-      System.out.println(a+b);   
+      System.out.println("The sum is " + a+b);   
     }
 }
